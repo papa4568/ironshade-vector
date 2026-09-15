339e318e10c3f55271dc32da4044dc842856d6b5
papa4568 <fpspro0421@gmail.com>
2026-09-15 18:23:06 -0500

    Load mobile combat readability overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './spaceCombat.css';
+import './mobileCombatReadability.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
